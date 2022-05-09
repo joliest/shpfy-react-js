cfbd7923b962a2cef3391b3c33aa971fe5f451f2
Joliboy <joliverestampador@gmail.com>
2022-05-09 10:07:54 +0800

    converted StoreLogin to func comp

--- src/components/StoreLogin.js
+++ src/components/StoreLogin.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
@@ -8,63 +8,56 @@ import {NGROK} from "../constants";
 import Products from "./Products";
 
 const MY_SHOPIFY_DOT_COM = '.myshopify.com';
-class StoreLogin extends React.Component {
-    state = {
-        storeName: 'sample-store-1ventory',
-        products: [],
+const StoreLogin = () => {
+    const [storeName, setStoreName] = useState('sample-store-1ventory');
+    const isButtonDisabled = !storeName;
+
+
+    const handleOnChange = (e) => {
+        setStoreName(e.target.value);
     }
 
-    handleOnChange = (e) => {
-        this.setState({ storeName: e.target.value });
-    }
-
-    handleClick = () => {
-        const { storeName } = this.state;
+    const handleClick = () => {
         if (storeName) {
             const url = `${NGROK}/shopify?shop=${storeName}.myshopify.com`;
             window.location.replace(url);
         }
     }
 
-    render() {
-        const { storeName } = this.state;
-        const isButtonDisabled = !storeName;
-        return (
-            <div className="store-login">
-                <Box component="form" noValidate autoComplete="on">
-                    <TextField
-                        id="outlined-basic"
-                        className="text-field"
-                        label="Enter Shopify store"
-                        variant="outlined"
-                        InputProps={{
-                            endAdornment: <InputAdornment position="end">{MY_SHOPIFY_DOT_COM}</InputAdornment>,
-                        }}
-                        InputLabelProps={{
-                            shrink: true
-                        }}
-                        value={storeName}
-                        onChange={this.handleOnChange}
-                    />
-                    <Box>
-                        <Button
-                            className="button"
-                            variant="contained"
-                            size="large"
-                            onClick={this.handleClick}
-                            disabled={isButtonDisabled}
-                        >
-                            Login to Sample Shopify App
-                        </Button>
-                    </Box>
-                </Box>
-                <Products
-                    storeName={storeName}
-                    isButtonDisabled={isButtonDisabled}
+    return (
+        <div className="store-login">
+            <Box component="form" noValidate autoComplete="on">
+                <TextField
+                    id="outlined-basic"
+                    className="text-field"
+                    label="Enter Shopify store"
+                    variant="outlined"
+                    InputProps={{
+                        endAdornment: <InputAdornment position="end">{MY_SHOPIFY_DOT_COM}</InputAdornment>,
+                    }}
+                    InputLabelProps={{
+                        shrink: true
+                    }}
+                    value={storeName}
+                    onChange={handleOnChange}
                 />
-            </div>
-        )
-    }
+                <Box>
+                    <Button
+                        className="button"
+                        variant="contained"
+                        size="large"
+                        onClick={handleClick}
+                        disabled={isButtonDisabled}
+                    >
+                        Login to Sample Shopify App
+                    </Button>
+                </Box>
+            </Box>
+            <Products
+                storeName={storeName}
+                isButtonDisabled={isButtonDisabled}
+            />
+        </div>
+    )
 }
-
 export default StoreLogin;
